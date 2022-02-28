f201ed51b42ab97ce3531b2a9d33a5f2a93dbfb8
Umut Can <umutcanagirtas@gmail.com>
2022-02-28 14:44:41 +0300

    Editing retry class

--- src/main/java/retry/RetryAnalyzer.java
+++ src/main/java/retry/RetryAnalyzer.java
@@ -1,18 +1,25 @@
 package retry;
 
+
 import org.testng.IRetryAnalyzer;
 import org.testng.ITestResult;
 
 public class RetryAnalyzer implements IRetryAnalyzer {
-    int counter = 0;
-    int retryLimit = 4;
+    private static final int MAX_TRY = 1;
+    private int count = 1;
 
+    @Override
     public boolean retry(ITestResult result) {
-
-        if(counter < retryLimit)
-        {
-            counter++;
-            return true;
+        if (!result.isSuccess()) {
+            if (count < MAX_TRY) {
+                count++;
+                result.setStatus(ITestResult.FAILURE);
+                return true;
+            } else {
+                result.setStatus(ITestResult.FAILURE);
+            }
+        } else {
+            result.setStatus(ITestResult.SUCCESS);
         }
         return false;
     }
